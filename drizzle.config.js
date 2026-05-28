cf892cbcff7bade9c8c073324883e518a1adf8f0
Pranav Gupta <ompranav2003@gmail.com>
2026-05-28 19:48:47 +0530

    drizzle+nepn backend setup 07:48pm 28may

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './utils/schema.jsx',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
