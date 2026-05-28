a716d90b66da29ae062eb7cf6a7ae796b23729fd
Pranav Gupta <ompranav2003@gmail.com>
2026-05-28 17:55:31 +0530

    sideNavBar Setup 05:5pm 28may

---
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -0,0 +1,70 @@
+"use client"
+import Image from 'next/image'
+import React, { useEffect } from 'react'
+import Dashboard from '../page'
+import { LayoutGrid, PiggyBank, ReceiptText, ShieldCheck } from 'lucide-react'
+import { UserButton } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
+function SideNav() {
+    const menuList=[
+        {
+            id:1,
+            name:'Dashboard',
+            icon: LayoutGrid,
+            path: '/dashboard'
+        },
+        {
+            id:2,
+            name:'Budgets',
+            icon: PiggyBank,
+            path: '/dashboard/budgets'
+        },
+        {
+            id:3,
+            name:'Expenses',
+            icon: ReceiptText,
+            path: '/dashboard/expenses'
+        },
+        {
+            id:4,
+            name:'Upgrade',
+            icon: ShieldCheck,
+            path: '/dashboard/upgrade'
+        }
+    ]
+
+    const path=usePathname();
+
+    useEffect(() => {
+        console.log(path)
+    },[path])
+  return (
+    <div className='h-screen p-5 border shadow-sm'>
+        <Image src={'./logo.svg'}
+        alt='logo'
+        width={160}
+        height={100}/>
+
+        <div className='mt-5'>
+            {menuList.map((menu, index) => (
+                <Link href={menu.path}>
+                <h2 
+                className={`flex gap-2 items-center text-gray-500 font-medium p-5 mb-2 cursor-pointer rounded-md hover:bg-gray-100  hover:text-black 
+                    ${path == menu.path && 'bg-gray-100 text-primary'}`}>
+                    <menu.icon/>
+                    {menu.name}
+                </h2>
+                </Link>
+            ))}
+        </div>
+        <div className='fixed bottom-10 p-5 flex gap-2 items-center'>
+            <UserButton/>
+            Profile
+        </div>
+    </div>
+  )
+}
+
+export default SideNav
